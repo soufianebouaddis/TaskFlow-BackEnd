720cf92294d03076bec503858c653102d06f955a
soufianeBouaddis <fousenaibdisbdis@gmail.com>
2025-06-20 00:12:59 +0100

    Adding assigning task to developer by the manager

--- src/main/java/os/org/taskflow/manager/controller/ManagerController.java
+++ src/main/java/os/org/taskflow/manager/controller/ManagerController.java
@@ -1,4 +1,44 @@
 package os.org.taskflow.manager.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import os.org.taskflow.common.ApiResponseEntity;
+import os.org.taskflow.manager.service.ManagerService;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/manager")
 public class ManagerController {
+    private final ManagerService managerService;
+
+    public ManagerController(ManagerService managerService) {
+        this.managerService = managerService;
+    }
+
+    @PostMapping("/{taskId}/{developerId}")
+    @PreAuthorize("hasAuthority('MANAGER')")
+    public ApiResponseEntity<?> assignedTaskToDeveloper(@PathVariable Long taskId, @PathVariable UUID developerId){
+        try{
+            return new ApiResponseEntity(
+                    Instant.now(),
+                    true,
+                    "Task assigned to developer successfully",
+                    HttpStatus.OK,
+                    managerService.assignedTaskToDeveloper(taskId,developerId)
+            );
+        }catch (Exception ex){
+            return new ApiResponseEntity<>(
+                    Instant.now(),
+                    false,
+                    "Error : "+ex.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    null);
+        }
+    }
 }
--- src/main/java/os/org/taskflow/manager/service/impl/ManagerServiceImpl.java
+++ src/main/java/os/org/taskflow/manager/service/impl/ManagerServiceImpl.java
@@ -1,4 +1,36 @@
 package os.org.taskflow.manager.service.impl;
 
-public class ManagerServiceImpl {
+import org.springframework.stereotype.Service;
+import os.org.taskflow.developer.entity.Developer;
+import os.org.taskflow.developer.repository.DeveloperRepository;
+import os.org.taskflow.manager.service.ManagerService;
+import os.org.taskflow.task.entity.Task;
+import os.org.taskflow.task.repository.TaskRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ManagerServiceImpl implements ManagerService {
+    private final DeveloperRepository developerRepository;
+    private final TaskRepository taskRepository;
+
+    public ManagerServiceImpl(DeveloperRepository developerRepository, TaskRepository taskRepository) {
+        this.developerRepository = developerRepository;
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public Optional<Task> assignedTaskToDeveloper(Long taskId, UUID developerId) {
+        Optional<Developer> developer = this.developerRepository.findById(developerId);
+        Optional<Task> task = this.taskRepository.findById(taskId);
+        if(developer.isPresent() && task.isPresent()){
+            developer.get().getTasks().add(task.get());
+            task.get().setDeveloper(developer.get());
+            this.developerRepository.save(developer.get());
+            this.taskRepository.save(task.get());
+            return task;
+        }
+        return Optional.empty();
+    }
 }
